6ea899b15ac575cf1206358cd219cc8a88b8336a
AZCOM <145325495+yann-andronio@users.noreply.github.com>
2024-09-17 15:38:49 +0300

    maj navbar

---
+++ src/components/doing/Doing.tsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react/jsx-runtime"
+
+const Doing: React.FC = () => {
+    return (
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+export default Doing
--- src/components/rigth/Rigth.tsx
+++ src/components/rigth/Rigth.tsx
@@ -10,9 +10,16 @@ const Rigth: React.FC = () => {
             <div className={`${s.navbar} relative`}>
                <Navbar />
             </div>
-            <div className={`${s.description} p-4 mt-16`}>
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam perspiciatis ex neque sit est laboriosam ratione placeat quasi iusto amet!</p>
-               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, labore! Voluptas recusandae ratione ducimus eius? Tempora, hic, distinctio molestiae ipsum dolorum cupiditate tempore, soluta animi aperiam alias ad perspiciatis molestias.</p>
+            <div className={`${s.description} p-4 text-gray-400`}>
+               <h1 className={`${s.titleabout} relative font-bold text-4xl text-white py-4`}>À propos de moi</h1>
+               <div className={`${s.descletter} mt-6`}>
+               <p className="text-base leading-relaxed mb-4">
+                     Je suis développeur full stack JavaScript, et j'adore créer des sites web et des applications. Je travaille avec des technologies comme React.js pour le front-end et Node.js pour le back-end, et j'utilise MongoDB pour gérer les données. Vous pouvez voir certains de mes projets dans mon portfolio. 
+                  </p>
+                  <p className="text-base leading-relaxed">
+                     Je suis aussi disponible pour des missions freelance ou pour travailler sur des projets intéressants. N'hésitez pas à me contacter si vous avez une idée de projet ou si vous voulez discuter d'une collaboration.
+                  </p>
+               </div>
             </div>
          </div>
 
