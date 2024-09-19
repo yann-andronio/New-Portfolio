af469985d195a595511e25c9b8910abb538652d1
AZCOM <145325495+yann-andronio@users.noreply.github.com>
2024-09-19 20:50:26 +0300

    design de cardproject

--- src/components/projectportfolio/Projectportfolio.tsx
+++ src/components/projectportfolio/Projectportfolio.tsx
@@ -3,6 +3,7 @@ import s from "./projectportfolio.module.css";
 import { useState } from "react";
 import Dataporfolioproject from "../../data/Dataporfolioproject";
 import { projectItems } from "../../data/Dataporfolioproject";
+import { FiExternalLink } from 'react-icons/fi';
 
 const Projectportfolio: React.FC = () => {
     const [Dataptojectfilter, Setdataprojectfilter] = useState<projectItems[]>(Dataporfolioproject);
@@ -16,51 +17,56 @@ const Projectportfolio: React.FC = () => {
 
     return (
         <Fragment>
-        
+
             <div className={`${s.tabfilter} flex gap-8`}>
-                <h1 
-                    onClick={() => Filtebytype("Tout")} 
+                <h1
+                    onClick={() => Filtebytype("Tout")}
                     className={` font-semibold text-xl cursor-pointer ${Typeactive === "Tout" ? s.active : s.nonactivve}`}
                 >
                     Tout
                 </h1>
-                <h1 
-                    onClick={() => Filtebytype("Web")} 
-                    className={` font-semibold text-xl cursor-pointer ${Typeactive === "Web" ? s.active  : s.nonactivve}`}
+                <h1
+                    onClick={() => Filtebytype("Web")}
+                    className={` font-semibold text-xl cursor-pointer ${Typeactive === "Web" ? s.active : s.nonactivve}`}
                 >
                     Site web
                 </h1>
-                <h1 
-                    onClick={() => Filtebytype("Mobile")} 
+                <h1
+                    onClick={() => Filtebytype("Mobile")}
                     className={` font-semibold text-xl cursor-pointer ${Typeactive === "Mobile" ? s.active : s.nonactivve}`}
                 >
                     Application mobile
                 </h1>
-                <h1 
-                    onClick={() => Filtebytype("Jeux vidéo")} 
+                <h1
+                    onClick={() => Filtebytype("Jeux vidéo")}
                     className={` font-semibold text-xl cursor-pointer ${Typeactive === "Jeux vidéo" ? s.active : s.nonactivve}`}
                 >
                     Jeux vidéo
                 </h1>
             </div>
 
-           
-            <div className={`${s.projectList}`}>
+
+            <div className={`${s.projectList} mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5`}>
                 {Dataptojectfilter.map((item, index) => (
-                    <div key={index} className={s.projectCard}>
-                        <img src={`./image/projet/${item.image}.png`} alt={item.name} className={s.projectImage} />
-                        <h2 className={s.projectName}>{item.name}</h2>
-                        <h3 className={s.projectType}>{item.type}</h3>
-                        <div className={s.icons}>
-                            {item.langageIcons.map((LangIcon, i) => {
+                    <div key={index} className={`${s.projectCard} rounded-3xl p-4`}>
+                        <img src={`./image/projet/${item.image}.png`} alt={item.name} className={`${s.projectImage} rounded-lg`} />
+                        <h2 className={`${s.projectName} font-bold text-2xl text-gray-300 mt-2`}>{item.name}</h2>
+                        <h3 className={`${s.projectType} text-base `}>{item.type}</h3>
+                        <div className={`${s.icons} flex gap-5 mt-4 `}>
+                            {item.langageIcons.map((LangIcon, index) => {
                                 const IconComponent = LangIcon.icon;
                                 return (
-                                    <IconComponent key={i} size={30} style={{ color: LangIcon.color }} />
+                                    <IconComponent key={index} size={30} style={{ color: LangIcon.color }} />
                                 );
                             })}
                         </div>
-                        <a href={item.link} className={s.projectLink} target="_blank" rel="noopener noreferrer">
-                            Voir le projet
+                        <a
+                            href={item.link}
+                            className={`${s.linkvoir} mt-4 inline-flex items-center`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Voir le projet <FiExternalLink className="ml-2" size={18} />
                         </a>
                     </div>
                 ))}
--- src/components/rightportfolio/Rightportfolio.tsx
+++ src/components/rightportfolio/Rightportfolio.tsx
@@ -3,8 +3,6 @@ import s from "./rigthportfolio.module.css"
 import Navmobile from "../navmobile/Navmobile"
 import Cvmobile from "../cvmobile/Cvmobile"
 import Navbar from "../navbar/Navbar"
-import Experience from "../experience/Experience"
-import Skills from "../skills/Skills"
 import Projectportfolio from "../projectportfolio/Projectportfolio"
 
 
@@ -28,11 +26,7 @@ const Rightportfolio: React.FC = () => {
                         <div className={`${s.educationbox} text-white leading-relaxed mb-4`}>
                             <Projectportfolio />
                         </div>
-                        <div className={`${s.experiencebox} text-white leading-relaxed mb-4`}>
-                            <Experience />
-                        </div>
-
-                        <Skills />
+                     
 
 
                     </div>
